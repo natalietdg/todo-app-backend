07d340a5bf43f4ec782f8432c7d7eaf31b876e75
Natalie Leong <natalie.leong@paywatchglobal.com>
2025-05-10 13:55:51 +0800

    feat: add deleted column to users table

---
+++ server/migrations/20250510054648_add-deleted-column-users-table.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.alterTable('users', function(table) {
+    table.boolean('deleted').defaultTo(false);
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.alterTable('users', function(table) {
+        table.dropColumn('deleted');
+    })
+};
