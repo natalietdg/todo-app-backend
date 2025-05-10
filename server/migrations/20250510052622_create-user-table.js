89b8aa9c6e0c6ef3b64853bd5723949f001d2334
Natalie Leong <natalie.leong@paywatchglobal.com>
2025-05-10 13:35:36 +0800

    feat: add users table

---
+++ server/migrations/20250510052622_create-user-table.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('users', function(table) {
+    table.string('id');
+    table.string('name');
+    table.string('status');
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable('users');
+};
